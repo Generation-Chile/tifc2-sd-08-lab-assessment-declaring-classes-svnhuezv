9d54e1920cc8748d4ebcd62ad9991a2cdb906f34
Raúl Ernesto Sanhueza Manríquez <resm5233@gmail.com>
2025-04-15 12:44:37 -0400

    Trabajo Class Terminado

--- task4.js
+++ task4.js
@@ -1,7 +1,14 @@
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
-  
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
+  }
+  levelUp() {
+    this.level += 1;
+    console.log(`${this.name} has leveled up to Level ${this.level}!`);
+  }
   
 }
--- task5.js
+++ task5.js
@@ -1,8 +1,16 @@
 export class Player {
-  constructor() {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+  }
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
+  }
+  levelUp() {
+    this.level += 1;
+    console.log(`${this.name} has leveled up to Level ${this.level}!`);
+  }
+  experienceUp() {
     
   }
- 
-  }
-
-  
+}
